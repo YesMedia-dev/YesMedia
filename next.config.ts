ce4a899e7b8acce193f2fb0f62619a3959e713a5
chrys-pl <chrys@yesmedia.us>
2025-05-06 02:57:01 -0700

    voy a morir

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true, // optional
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
+
+
