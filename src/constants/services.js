b5f374adc4151c50da59d0557f4d95d37963cf36
Benji0506 <behenriq0506@gmail.com>
2025-05-13 02:25:15 -0700

    Added services type and constants

---
+++ src/constants/services.js
@@ -0,0 +1,44 @@
+const SERVICES = [
+  {
+    title: "Overview",
+    description:
+      "Our experienced therapy team is committed to providing superior physical, occupational and speech therapy in an environment conducive to healing.",
+    image: "/assets/services1.png",
+    link: "/services/overview",
+    size: 110,
+  },
+  {
+    title: "Rehabilitation",
+    description:
+      "Our experienced therapy team is committed to providing superior physical, occupational and speech therapy in an environment conducive to healing.",
+    image: "/assets/services2.png",
+    link: "/services/rehabilitation",
+    size: 80,
+  },
+  {
+    title: "Skilled Nursing",
+    description:
+      "We provide individualized 24-hour skilled care and rehabilitation services in a comfortable and family-friendly environment.",
+    image: "/assets/services3.png",
+    link: "/services/nursing",
+    size: 130,
+  },
+  {
+    title: "Social Services",
+    description:
+      "Social Services provide essential information, manage requests and concerns, help in the care planning and discharge planning for each patient.",
+    image: "/assets/services4.png",
+    link: "/services/social",
+    size: 140,
+  },
+  {
+    title: "Activities",
+    description:
+      "We create positive and uplifting social programs and individualized activities to match patient needs and capabilities.",
+    image: "/assets/services5.png",
+    link: "/services/activities",
+    size: 130,
+  },
+];
+
+export { SERVICES };
---
+++ src/types/services.ts
@@ -0,0 +1,7 @@
+export type Services = {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+    size: number;
+}
