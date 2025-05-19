39e742e45bf469eb11abb150d4505329fc701322
Benji0506 <behenriq0506@gmail.com>
2025-05-18 23:58:09 -0700

    Updated positioning

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,10 +7,10 @@ const Banner = () => {
   const { t } = useTranslation("common");
 
   return (
-    <section className="mobile-banner relative h-[400px] bg-fixed bg-contain md:bg-cover bg-center text-white flex items-center justify-center px-4 text-center mt-20">
+    <section className="mobile-banner relative h-[400px] bg-cover bg-center md:bg-fixed md:bg-cover text-white flex items-center justify-center px-4 text-center mt-20">
       <div className="bg-black/50 w-full h-full absolute top-0 left-0"></div>
 
-      <div className="relative z-10 text-white max-w-3xl">
+      <div className="relative z-10 text-white max-w-3xl -mt-8 md:mt-0">
         <h2 className="text-2xl md:text-3xl font-semibold mb-2">&quot;{t("bannerQuote")}&quot;</h2>
         <p className="text-sm md:text-base font-light">{t("bannerAttribution")}</p>
       </div>
